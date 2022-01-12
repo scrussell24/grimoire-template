97a541d1add21bb9ac4654524b3a7cb75bb3b673
Scott Russell <scrussell24@gmail.com>
2022-01-11 23:20:56 -0600

    use updated grimoire template

--- src/templates.py
+++ src/templates.py
@@ -1,42 +1,13 @@
-from typing import List
-
-from grimoire.templates import link
+from grimoire.templates import default_page
 from grimoire.utils import make_decorator
-from hype import *
+from hype import Div, P
 
 
 @make_decorator
-def template(fn, state: str, *opts: List[int]):
+@default_page("Grimoire Story")
+def template(fn, state, *opts):
     paragraphs, options, state = fn(state, *opts)
-    return (
-        Doc(
-            Html(
-                Head(
-                    Title("Grimoire Story"),
-                    Meta(charset="utf-8"),
-                    Meta( name="viewport", content="width=device-width, initial-scale=1"),
-                    Link(rel="stylesheet", href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css"),
-                    Style("""
-ul {
-    padding-left: 0px !important;
-}
-
-li {
-    list-style: none !important;
-}""")
-                ),
-                Body(
-                    Main(
-                        *[Section(P(p)) for p in paragraphs],
-                        Section(
-                        Ul(*[Li(link(o[0], o[1])) for o in options]),
-                        ),
-                        _class="container",
-                    ),
-                ),
-                lang="en",
-                data_theme="dark"
-            )
-        ),
-        state,
+    content = Div(
+        *[P(p) for p in paragraphs],
     )
+    return content, options, state
